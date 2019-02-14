797112bf6b34f7b11f6da231b4c57ab5506479d9
ericzya <ericzya@outlook.com>
2019-02-14 10:52:00 +0800

    Merge branch 'master' of https://github.com/Ericzya/LeetCode
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

---
+++ src/a3/LengthOfLongestSubstring/LengthOfLongestSubstring.java
@@ -0,0 +1,30 @@
+package a3.LengthOfLongestSubstring;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LengthOfLongestSubstring {
+    public static void main(String[] args) {
+        System.out.print(lengthOfLongestSubstring("a;lfsj;lajsdjaljfljjjjjjjjjjkjkkkeeeeeeeee"));
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        char[] elements = s.toCharArray();
+        int result = 0;
+        int startPos = 0;
+        for (int i = 0; i < elements.length; i++) {
+            startPos = i;
+            Set<Character> hashSet = new HashSet<>();
+            for (int j = i; j < elements.length; j++) {
+                if (hashSet.contains(elements[j])) {
+                    startPos = j;
+                    continue;
+                } else {
+                    result = Math.max(j - startPos + 1, result);
+                    hashSet.add(elements[j]);
+                }
+            }
+        }
+        return result;
+    }
+}
